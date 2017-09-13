6f48842b88dd53f10dc8dc7cafedbe9d55640d33
KorJuk69 <korjuk1@gmail.com>
2017-09-13 18:29:27 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,25 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        int pol=0, otr=0, nul=0;
+
+        for(int i=0;i<n;i++){
+            int x = scanner.nextInt();
+            if(x>0){
+                pol++;
+            }
+            else{
+                if(x<0){
+                    otr++;
+                }
+                else {
+                    nul++;
+                }
+            }
+        }
+
+        System.out.print(nul + " " + pol + " " + otr);
 
     }
 }
