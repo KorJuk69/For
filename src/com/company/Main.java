a8e10e175edba02e683e257617d74889839e18ef
KorJuk69 <korjuk1@gmail.com>
2017-09-13 17:44:30 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int x = scanner.nextInt();
+        int kolvo=0;
+
+        for(int i=1;i<=x;i++){
+
+            if(x%i==0){
+                kolvo++;
+            }
+
+        }
+
+        System.out.print(kolvo);
 
     }
 }
