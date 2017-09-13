f2ffc18013201be46fc4b08114781953781f919b
KorJuk69 <korjuk1@gmail.com>
2017-09-13 17:19:43 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +11,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
+
+        while (a<=b){
+
+            int c = (int)sqrt(a);
+            if(c*c==a){
+                System.out.print(a + " ");
+            }
+            a++;
+
+        }
 
     }
 }
