c2f818c333c602708505ffdbf3238e34b8e3e6ad
KorJuk69 <korjuk1@gmail.com>
2017-09-13 18:21:46 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            int x = scanner.nextInt();
+            sum = sum + x;
+        }
+
+        System.out.print(sum);
 
     }
 }
