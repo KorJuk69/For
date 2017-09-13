25107a68b2054890d6dd3337d7972ac658077747
KorJuk69 <korjuk1@gmail.com>
2017-09-13 19:48:52 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,19 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int b=0;
+        int chislo=0;
+
+        for (int i=1;i<=a;i++){
+
+            if(i>b){
+                chislo++;
+                b=b+chislo;
+            }
+
+            System.out.print(chislo + " ");
+
+        }
 
     }
 }
